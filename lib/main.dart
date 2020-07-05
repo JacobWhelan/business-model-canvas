e03e061ad4c11db4d541c592c170b7d67342ea50
Lleno <launiv3rse@gmail.com>
2020-07-05 23:55:13 +0100

    Added main App and question screen skeleton.

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,28 @@
+import 'package:business_model_canvas/routes.dart';
+import 'package:business_model_canvas/theme/style.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+void main() {
+  runApp(BusinessApp());
+}
+
+class BusinessApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (BuildContext context) {},
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Business Model Canvas",
+        theme: appTheme(),
+        routes: routes,
+        initialRoute: "questions",
+      ),
+    );
+  }
+}
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,1 +1,7 @@
+import 'package:flutter/widgets.dart';
 
+import 'file:///C:/Users/launi/OneDrive/Desktop/UCD/Flutter/business_model_canvas/lib/screens/questions/Questions.dart';
+
+final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
+  "questions": (BuildContext context) => QuestionScreen(),
+};
