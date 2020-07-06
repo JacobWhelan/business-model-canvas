0a2d47cf2b752dd9239ddcf4be4c31016bfd5ac1
Lleno <launiv3rse@gmail.com>
2020-07-06 20:42:11 +0100

    Fixed import statement

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,5 @@
+import 'package:business_model_canvas/screens/questions/Questions.dart';
 import 'package:flutter/widgets.dart';
-
-import 'file:///C:/Users/launi/OneDrive/Desktop/UCD/Flutter/business_model_canvas/lib/screens/questions/Questions.dart';
 
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
   "questions": (BuildContext context) => QuestionScreen(),
