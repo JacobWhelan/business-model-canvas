94b7b1455696f735a22e4fe567005d564168f091
Lleno <launiv3rse@gmail.com>
2020-07-09 20:35:18 +0100

    Added CardTabs as the body of questions.dart

--- lib/screens/questions/questions.dart
+++ lib/screens/questions/questions.dart
@@ -1,3 +1,4 @@
+import 'package:business_model_canvas/screens/questions/components/card_tabs.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -13,6 +14,7 @@ class _QuestionScreenState extends State<QuestionScreen> {
       create: (BuildContext context) {},
       child: Scaffold(
         appBar: AppBar(),
+        body: CardTabs(),
       ),
     );
   }
