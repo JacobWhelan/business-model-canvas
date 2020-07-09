5a720e68a3f1716b28aad2dd0d7e2a7d242ff604
jacobwhelan <jacobwhelan12@gmail.com>
2020-07-09 16:14:12 +0100

    Added card class

---
+++ lib/screens/questions/components/cards.dart
@@ -0,0 +1,34 @@
+import 'package:business_model_canvas/screens/questions/components/answers.dart';
+import 'package:flutter/material.dart';
+
+class QuestionCard extends StatefulWidget {
+  final String questionText;
+
+  const QuestionCard(this.questionText);
+
+  @override
+  _QuestionCardState createState() => _QuestionCardState();
+}
+
+class _QuestionCardState extends State<QuestionCard> {
+  //move this list into 'assets' file and populate
+  //static const List<String> _questions = ['1?', '2?', '3?', '4?', '5?'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(widget.questionText,
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.underline)),
+          AnswerField()
+        ],
+      ),
+    );
+  }
+}
