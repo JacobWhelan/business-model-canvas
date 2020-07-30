da5512fcf8d23017c1ac2f394a88637fd2f488a5
Lleno <launiv3rse@gmail.com>
2020-07-30 02:35:23 +0100

    Added Button styling to ThemeData

--- lib/theme/style.dart
+++ lib/theme/style.dart
@@ -1,3 +1,11 @@
 import 'package:flutter/material.dart';
 
-ThemeData appTheme() => ThemeData();
+ThemeData appTheme() => ThemeData(
+      primaryColor: Colors.blue,
+      accentColor: Colors.white,
+      buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blue, textTheme: ButtonTextTheme.accent),
+      appBarTheme: AppBarTheme(
+        color: Colors.blueAccent,
+      ),
+    );
